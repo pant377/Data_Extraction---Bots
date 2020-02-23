fb3059754de18bfae87e6ee25f13f2038b0a98d3
pant377 <49340326+pant377@users.noreply.github.com>
2020-02-23 10:49:46 +0200

    Add files via upload

--- wiki.py
+++ wiki.py
@@ -1,15 +1,47 @@
 from bs4 import BeautifulSoup
 import requests
-import re
-html_page = requests.get("https://el.wikipedia.org/wiki/Διεθνές_Πανεπιστήμιο_της_Ελλάδος")
+import time
+urls = []
+h1s = []
+urlM = "https://el.wikipedia.org/wiki/Διεθνές_Πανεπιστήμιο_της_Ελλάδος"
+html_page = requests.get(urlM)
 soup = BeautifulSoup(html_page.content, 'html.parser')
+links = soup.find_all('a')
 
-for link in soup.findAll('a'):
-    pages = link.get('href')
-    for j in pages:
-        page2 = reguests.get(j)
-        print(page2)
+def manage_links(links):
+    urls = []
+    file2 = open("./links.txt","a", encoding='utf-8')
+    for link in links:
+        try:
+            url = f"https://el.wikipedia.org/{link['href']}" 
+            urls.append(url)
+            file2.write(url +'\n')
+        except:
+            pass       
+    manage_urls(urls)          
 
+def manage_urls(urls):
+    links2 = []
+    names = open("./names.txt","a",encoding='utf-8')
+    for i in urls:
+        try:
+            page = requests.get(i)
+            soup = BeautifulSoup(page.content, 'html.parser')
+            time.sleep(0.2)
+            h1 = soup.find(id="firstHeading").text
+            l = soup.find_all('a')
+            links2.append(l)
+            h1s.append(h1)
+            print(h1)
+            names.write(h1 +'\n')
+        except:
+            pass
+    manage_links(links2)
 
+manage_links(links)
 
-
+#fruits = ['apple','banana']
+#for num,fruit in enumerate(fruits):
+#  print(num)
+#  print(fruit)
+#Result: 0 apple 1 banana
