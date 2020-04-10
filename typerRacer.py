b6f9494f79bd3a4dff22d4167b4bd5d3877dd484
pant377 <49340326+pant377@users.noreply.github.com>
2020-04-10 22:25:24 +0300

    Delete typerRacer.py

--- typerRacer.py
+++
@@ -1,9 +0,0 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver import Firefox
-import time
-
-driver = webdriver.Firefox(executable_path='/usr/bin/geckodriver')
-driver.get('http://inventwithpython.com')
-time.sleep(1)
-driver.quit()
