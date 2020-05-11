6e124bc68c87ff381e7633c6e90800888b971d23
pant377 <pantpap15@gmail.com>
2020-05-11 16:25:35 +0300

    new coronacases

--- coronacases.py
+++ coronacases.py
@@ -1,17 +1,35 @@
 from bs4 import BeautifulSoup as bs
 import requests
-from lxml import html
 import time
-
-soup = bs(requests.get('https://www.worldometers.info/coronavirus/').content,'html.parser')
- #tree = html.fromstring(requests.get('https://www.worldometers.info/coronavirus/').content)
+import pandas as pd
+countrylist = []
+countryc = []
+dpcountry = []
+headers = {"User-Agent": 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:69.0) Gecko/20100101 Firefox/69.0'}
+page = requests.get("https://www.worldometers.info/coronavirus/", headers=headers)
+soup = bs(page.content,'html.parser')
 cases = soup.findAll(id='maincounter-wrap')
 infected = cases[0].find('span').text.replace(",","")
-deths = cases[1].find('span').text.replace(",","") #na thimase oti se auto pou vrikes mporeis na kaneis find
-#withxpath = tree.xpath('//*[@id="maincounter-wrap"]/div/span/text()')
-#for i in withxpath:
-   # number = i.replace(' ','').replace(',','.') 
-   # print(float(number))
+deths = cases[1].find('span').text.replace(",","") 
+table = soup.find(id="main_table_countries_today")
+countrys = table.find_all((("a", {"class":"mt_a"})))
+for i in countrys:
+   try:
+      txt = i.text
+      cpc = i.next.next.next.text
+      dpc = i.next.next.next.next.next.next.next.next.text
+      print(txt," -- ",cpc," -- ",dpc)
+      countrylist.append(txt)
+      countryc.append(cpc)
+      dpcountry.append(dpc)
+   except:
+      print("")   
+tablecsv = pd.DataFrame({'Country':countrylist[0:],
+                         'Cases':countryc[0:],
+                         'Deaths':dpcountry[0:]})      
+
+tablecsv.to_csv("C:/Users/pantelis/Desktop/file.csv")                         
+                         
 persent = (float(deths)/float(infected))*100
 print("Cases: "+infected+" |Deaths: "+deths+" |Persentage of deaths: "+str(persent)[0:5]+" %")
-time.sleep(10)
+#time.sleep(10)
