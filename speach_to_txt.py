c51e68706c09ac35a07adad0aed2283c53bbfdb1
pant377 <pantpap15@gmail.com>
2020-07-08 15:02:06 +0300

    testing voice comands

--- speach_to_txt.py
+++ speach_to_txt.py
@@ -8,18 +8,16 @@ def speak(txt):
     engine = pyttsx3.init()
     engine.say(txt)
     engine.runAndWait()
-
 def get_audio():
     r = sr.Recognizer()
     with sr.Microphone(device_index=2) as source:
-        r.adjust_for_ambient_noise(source,duration=1)
+        r.adjust_for_ambient_noise(source,duration=0.5)
         print("Speak Please ..")
-        audio = r.listen(source)
+        audio = r.listen(source, timeout=5)
         said = ""
         try:
             said = r.recognize_google(audio) 
         except Exception as e:
             print("Exception:",str(e))    
-
     return said.lower()        
 print(get_audio())
